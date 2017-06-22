6743bb0c2fa52fd0903b711749e0345b616060df
Isak09 <estecorreonoesprofesional@gmail.com>
2017-06-22 11:18:11 +0200

    Update Redireccion.cmd

--- Redireccion.cmd
+++ Redireccion.cmd
@@ -7,6 +7,7 @@
 :: En este ejemplo vamos a escoger la lista de archivos de un directorio
 :: escribiendo "dir" y despues el simbolo ">" seguido del nombre del archivo,
 :: en este caso "ejemplo.txt"
+:: Y si lo prefieres podemos comprobarlo a la vez que lo redireccionamos con "pipe"
 
 dir > ejemplo.txt | more
 
